815ad22ecdd7df52149ab4555b9aa2f3ea654c84
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:42:02 +0530

    assg file

---
+++ src/allAppJsFiles/StyledComponents/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import Sidebar from './components/StyledComponents/Sidebar';
+import BlackNavBar from './components/StyledComponents/BlackNavBar';
+// import NavBar from './components/NavBar';
+
+function App() {
+  return (
+    <>
+      {/* <NavBar/> */}
+      <BlackNavBar />
+      {/* <Sidebar /> */}
+    </>
+  );
+}
+
+export default App;
+
