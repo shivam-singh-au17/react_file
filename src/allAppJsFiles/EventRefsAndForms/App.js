b0e8440a90403e2847649e82cfb3208eacddf96e
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:39:51 +0530

    assg file

---
+++ src/allAppJsFiles/EventRefsAndForms/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import ConditionalRendering from './components/EventRefsAndForms/ConditionalRendering';
+
+function App() {
+  return (
+    <div className="myContainer">
+      <div className="main">
+        <ConditionalRendering />
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
