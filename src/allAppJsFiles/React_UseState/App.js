3643700114652cc8e58b53082777218b0cbff2eb
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:40:46 +0530

    assg file

---
+++ src/allAppJsFiles/React_UseState/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AddTask from './components/React_UseState/AddTask';
+import Counter from './components/React_UseState/Counter';
+
+function App() {
+  return (
+    <>
+      <Counter />
+      <AddTask />
+    </>
+  );
+}
+
+export default App;
