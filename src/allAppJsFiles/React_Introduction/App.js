dc024e96857fa4e8e0a4d09123b6341fdd43a99c
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:40:28 +0530

    assg file

---
+++ src/allAppJsFiles/React_Introduction/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import ImageName from './components/React_Introduction/ImageName';
+
+function App() {
+  return (
+    <>
+      <ImageName />
+    </>
+  );
+}
+
+export default App;
