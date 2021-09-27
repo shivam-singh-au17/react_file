76ce576cac428eac777e78c2231ea75c05b79238
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:39:11 +0530

    assg file

---
+++ src/allAppJsFiles/Axios/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import CovidAPI from './components/Axios/CovidAPI';
+import GitHubUsers from './components/Axios/GitHubUsers';
+// import AddTask from './components/Axios/AddTask';
+
+function App() {
+  return (
+    <>
+     <GitHubUsers/>
+      {/* <CovidAPI/> */}
+      {/* <AddTask/> */}
+    </>
+  );
+}
+
+export default App;
+
