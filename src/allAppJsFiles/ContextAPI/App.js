d1cc577627ef14524b822509dfa7d8704f2e3eb2
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:39:34 +0530

    assg file

---
+++ src/allAppJsFiles/ContextAPI/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+import { ContextAssg01 } from './components/ContextAPI/ContextAssg01';
+// import ChangeData from './components/ContextAPI/ChangeData';
+// import Formindex from './components/ContextAPI/Formindex';
+
+function App() {
+  return (
+    <>
+      {/* <Formindex />
+      <ChangeData /> */}
+      <ContextAssg01/>
+    </>
+  );
+}
+
+export default App;
+
