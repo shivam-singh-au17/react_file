5ad0a0874d160a39c30648415682ee2cd7ccdfcf
Shivam Singh <shivamsingh4458@gmail.com>
2021-09-27 13:40:12 +0530

    assg file

---
+++ src/allAppJsFiles/ListKeysConditional/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import AuthCondtion from './components/ListKeysConditional/AuthCondtion';
+
+
+function App() {
+    return (
+        <>
+            <AuthCondtion />
+        </>
+    );
+}
+
+export default App;
